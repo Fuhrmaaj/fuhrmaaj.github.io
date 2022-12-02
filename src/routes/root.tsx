88e39ce15d0f46f46b679ce8e42f880526f0432b
AJ Fuhrmann <fuhrmaaj@gmail.com>
2022-12-02 16:44:35 -0500

    set the default route

---
+++ src/routes/root.tsx
@@ -0,0 +1,31 @@
+import avatar from '../assets/avatar.jpg';
+
+export default function Root() {
+  return (
+      <header>
+          <ul className='nav-list'>
+            <li className='nav-link'>
+              <img alt="AJ Fuhrmann" src={avatar} id="avatar" />
+            </li>
+            <li className='nav-link'>
+              <p>AJ Fuhrmann</p>
+            </li>
+            <li className='nav-link'>
+              <p>About</p>
+            </li>
+            <li className='nav-link'>
+              <p>Work</p>
+            </li>
+            <li className='nav-link'>
+              <p>Example Projects</p>
+            </li>
+            <li className='nav-link'>
+              <p>Contact</p>
+            </li>
+            <li className='nav-link'>
+              <a href="https://github.com/Fuhrmaaj/fuhrmaaj.github.io" rel="author">The source code for this portfolio can be found on Github!</a>
+            </li>
+          </ul>
+      </header>
+  );
+};
