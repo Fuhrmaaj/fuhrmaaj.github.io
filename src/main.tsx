9ef54d385ffbfaae20f1139870b3707a8efe0ad3
AJ Fuhrmann <Fuhrmaaj@users.noreply.github.com>
2022-12-02 22:45:49 -0500

    init/router: Add react-router-dom
    Set up the initial routing with a generic navbar, comprehensive stylesheet, dark and light modes. Clean up the files generated by Vite and fix the title.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './index.css';
+import Root from './routes/root';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 )
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
